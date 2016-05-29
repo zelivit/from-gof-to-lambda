9dccf133479998406ed16847e4ae97df9eaef17e
zelivit <zelinskyi.s.v@gmail.com>
2016-05-29 22:00:30 +0300

    add doc

--- src/main/java/org/gof/examples/factorymethod/gof/TemplateMethodClient.java
+++ src/main/java/org/gof/examples/factorymethod/gof/TemplateMethodClient.java
@@ -4,8 +4,8 @@ public class TemplateMethodClient {
     public static void main(String[] args) {
         DocumentFactory factory = new JsonDocumentFactory();
         Document document = factory.create("USER");
-        document.addField("name", "Mikalai");
-        document.addField("surname", "Alimenkou");
+        document.addField("name", "Test");
+        document.addField("surname", "TestTest");
         System.out.println(document);
     }
 }
